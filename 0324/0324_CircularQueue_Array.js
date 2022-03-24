503893e2c3fea54384743862e8408d81c7329b0e
mieumje <66072832+mieumje@users.noreply.github.com>
2022-03-24 13:52:12 +0900

    0324_3_CircularQueue_Array

---
+++ 0324/0324_CircularQueue_Array.js
@@ -0,0 +1,50 @@
+class Queue{
+    constructor(maxSize){
+        this.maxSize = maxSize;
+        this.queue = [];
+        this.front = 0;
+        this.rear = 0;
+        this.size = 0;
+    }
+
+    enqueue(value){
+        if(this.isFull()){
+            console.log("Queue is full");
+            return
+        }
+        this.queue[this.rear] = value;
+        this.rear = (this.rear + 1) % this.maxSize;
+        this.size += 1;
+    }
+
+    dequeue(){
+        const value = this.queue[this.front];
+        delete this.queue[this.front];
+        this.front = (this.front + 1) % this.maxSize;
+        this.size -= 1;
+        return value
+    }
+
+    isFull(){
+        return this.size === this.maxSize
+    }
+
+    peek(){
+        return this.queue[this.front];
+    }
+}
+
+const queue = new Queue(4);
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(8);
+console.log(queue);
+
+console.log(queue.dequeue());
+console.log(queue);
+console.log(queue.peek());
+queue.enqueue(5);
+queue.enqueue(10);
+queue.enqueue(15);
+console.log(queue);
