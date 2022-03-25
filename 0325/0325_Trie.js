6b04b0037c7137cd84d4c49bf21435b82c1af549
mieumje <66072832+mieumje@users.noreply.github.com>
2022-03-25 17:30:06 +0900

    0325_Tire : has()
    insert()와 비슷한 로직
    있는 경우 계속 탐색 후 반복문 종료시 true return
    
    없는 경우 false return

--- 0325/0325_Trie.js
+++ 0325/0325_Trie.js
@@ -20,4 +20,20 @@ class Trie{
             currNode = currNode.children.get(chr);
         }
     }
+    
+    has(stringValue){
+        let currNode = this.root;
+        for(let chr of stringValue){
+            if(!currNode.children.has(chr)) return false
+            currNode = currNode.children.get(chr);
+        }
+        
+        return true
+    }
 }
+const trie = new Trie();
+trie.insert("cat");
+trie.insert("cap");
+console.log(trie.root.children.get('c').children);
+console.log(trie.has("cat"));
+console.log(trie.has("can"));
