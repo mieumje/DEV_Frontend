bf3f25d447bed1c5768870ca3f0efb202e84fc86
mieumje <66072832+mieumje@users.noreply.github.com>
2022-03-25 14:43:38 +0900

    0325_가장 먼 노드 : 풀이
    인접 리스트로 풀이

--- 0325/0325_가장 먼 노드.js
+++ 0325/0325_가장 먼 노드.js
@@ -1,23 +1,23 @@
 function solution(n, edge) {
     let answer;
-    let graph = Array.from(Array(n+1), () => Array(n+1).fill(0));
+    let graph = Array.from(Array(n+1), () => []);
     
-    for(let [node1,node2] of edge){
-        graph[node1][node2] = 1;
-        graph[node2][node1] = 1;
+    for(let [node1, node2] of edge){    // 인접 리스트로 그래프 표현
+        graph[node1].push(node2);
+        graph[node2].push(node1);
     }
     
-    let check = Array.from(Array(n+1), () => 0);
-    check[1] = 1;
+    let check = Array.from({length : n+1}, () => 0);
+    check[1] = 1; // 1번 노드부터 각 노드의 최단 경로를 찾기 위해 1번 시작 위치를 1로 초기화, 각 노드의 거리는 이전 노드의 거리 + 1
     let queue = [];
-    queue.push(1);
+    queue.push(1); // 1번 노드 부터 시작
     
     while(queue.length > 0){
         let startNode = queue.shift();
-        for(let i=1; i<=n; i++){
-            if(graph[startNode][i] && check[i]===0){
-                check[i] = check[startNode] + 1;
-                queue.push(i);
+        for(let endNode of graph[startNode]){
+            if(check[endNode] === 0) {
+                check[endNode] = check[startNode] + 1;
+                queue.push(endNode);
             }
         }
     }
@@ -25,7 +25,7 @@ function solution(n, edge) {
     let max = Math.max(...check);
     
     answer = check.filter(value => value === max);
-    return answer.length;
+    return answer.length
 }
 
 // n	vertex	                                                    return
