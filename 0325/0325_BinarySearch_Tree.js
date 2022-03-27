70abf76b801d64b9feddce2521947209531ac881
mieumje <66072832+mieumje@users.noreply.github.com>
2022-03-27 13:57:43 +0900

    0325_BinarySearch_Tree : insert()

---
+++ 0325/0325_BinarySearch_Tree.js
@@ -0,0 +1,48 @@
+class Node{
+    constructor(value){
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
+
+class binarySearchTree{
+    constructor(){
+        this.root = null;
+    }
+
+    insert(value){
+        const newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return;
+        }
+
+        let currNode = this.root;
+        while(currNode !== null){
+            if(currNode.value < value){
+                if(currNode.right === null){
+                    currNode.right = newNode;
+                    break;
+                }
+                currNode = currNode.right;
+            }else{
+                if(currNode.left === null){
+                    currNode.left = newNode;
+                    break;
+                }
+                currNode = currNode.left;
+            }
+        }
+    }
+}
+
+const tree = new binarySearchTree();
+tree.insert(5);
+console.log(tree);
+tree.insert(4);
+console.log(tree);
+tree.insert(7);
+console.log(tree);
+tree.insert(3);
+console.log(tree);
