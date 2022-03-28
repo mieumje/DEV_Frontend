b156a646049ddfdbcf7d88e13b99b9208d0832df
mieumje <66072832+mieumje@users.noreply.github.com>
2022-03-28 23:39:48 +0900

    0324_베스트앨범 풀이 : 인접 리스트

--- 0324/0324_베스트앨범.js
+++ 0324/0324_베스트앨범.js
@@ -45,4 +45,28 @@ function solution(genres, plays) {
 let genres = ["classic", "pop", "classic", "classic", "pop"];
 let plays = [500, 600, 150, 800, 2500];
 
-console.log(solution(genres,plays));
+// console.log(solution(genres,plays));
+
+function solution2(genres, plays){
+    const genreMap = new Map();
+
+    genres
+        .map((genre, index) => [genre, plays[index]])
+        .forEach(([genre, play], index) => {
+            const data = genreMap.get(genre) || { total : 0, songs : []};
+            genreMap.set(genre, {
+                total : data.total + play,
+                songs : [...data.songs, {play, index}]
+                    .sort((a,b) => b.play - a.play)
+                    .slice(0,2)
+            });
+        });
+    
+    return [...genreMap.entries()]
+        .sort((a,b) => b[1].total - a[1].total)
+        .flatMap((itme) => itme[1].songs)
+        .map((v) => v.index)
+    
+}
+
+console.log(solution2(genres,plays));
