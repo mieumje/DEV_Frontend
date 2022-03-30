159c74ff780b90c2dce97647f7d5201618cb558c
mieumje <66072832+mieumje@users.noreply.github.com>
2022-03-30 18:58:02 +0900

    0330_단어 퍼즐 : 효율성 실패
    정확성  테스트
    테스트 1 〉	통과 (0.27ms, 30.3MB)
    테스트 2 〉	통과 (0.31ms, 30.1MB)
    테스트 3 〉	통과 (0.11ms, 30.2MB)
    테스트 4 〉	통과 (0.43ms, 29.9MB)
    테스트 5 〉	통과 (0.73ms, 30.2MB)
    테스트 6 〉	통과 (0.26ms, 30.2MB)
    테스트 7 〉	통과 (1.40ms, 30.1MB)
    테스트 8 〉	통과 (283.94ms, 34.7MB)
    테스트 9 〉	통과 (287.70ms, 34.9MB)
    테스트 10 〉	통과 (284.55ms, 34.9MB)
    테스트 11 〉	통과 (282.16ms, 35MB)
    테스트 12 〉	통과 (0.28ms, 30.3MB)
    테스트 13 〉	통과 (0.13ms, 29.9MB)
    테스트 14 〉	통과 (0.31ms, 30.2MB)
    테스트 15 〉	통과 (0.31ms, 30.2MB)
    테스트 16 〉	통과 (0.25ms, 30.3MB)
    테스트 17 〉	통과 (0.13ms, 30.1MB)
    테스트 18 〉	통과 (0.14ms, 30.2MB)
    테스트 19 〉	통과 (0.29ms, 30.3MB)
    효율성  테스트
    테스트 1 〉	실패 (시간 초과)
    테스트 2 〉	실패 (시간 초과)
    테스트 3 〉	실패 (시간 초과)
    테스트 4 〉	실패 (시간 초과)
    테스트 5 〉	실패 (시간 초과)
    채점 결과
    정확성: 59.4

---
+++ 0330/단어 퍼즐.js
@@ -0,0 +1,33 @@
+function solution(strs, t) {
+    let answer = 0;
+    let l = t.length;
+    const dp = Array.from({length : l + 1}, ()=> 0);
+    let strsSet = new Set(strs);
+
+    for(let i=1; i<l + 1; i++){
+        dp[i] = 20000;
+        // console.log(t.slice(0,i)) // 현재 만들 단어
+        for(let j=1; j<i + 1; j++){
+            const start = i - j, end = i;
+            // console.log(t.slice(start, end))    // 확인 할 단어
+            if(strsSet.has(t.slice(start, end))){
+                dp[i] = Math.min(dp[i], dp[start] + 1);
+            }
+            console.log(dp)
+        }
+    }
+
+    answer = dp[l] !== 20000 ? dp[l] : -1
+
+    return answer;
+}
+
+// 입출력 예
+// strs	                                t	        result
+// ["ba","na","n","a"]	                "banana"	3
+// ["app","ap","p","l","e","ple","pp"]	"apple"	    2
+// ["ba","an","nan","ban","n"]	        "banana"	-1
+let strs = ["ba","na","n","a"];
+let t = "banana";
+console.log(solution(strs,t))
+console.log(t.slice(0,6))
