fdead995f0e640f91ee2e4e1afcf566e9234d6ca
mieumje <66072832+mieumje@users.noreply.github.com>
2022-04-04 13:33:23 +0900

    0404_Simple Todo List Component : Refactor
    Object Destructuring(구조분해할당) 활용

--- 0404/src/TodoList.js
+++ 0404/src/TodoList.js
@@ -1,16 +1,15 @@
 // params.$target - 해당 컴포넌트가 추가될 DOM
 // params.initialState - 해당 컴포넌트의 초기 상태
-function TodoList(params){
+function TodoList({$target, initialState}){
     const $todoList = document.createElement('div');
-    const $target = params.$target;
     $target.appendChild($todoList);
 
-    this.state = params.intialState;
+    this.state = initialState;
 
     this.render = () => {
         $todoList.innerHTML = `
         <ul>
-            ${this.state.map(todo => `<li>${todo.text}</li>`).join('')}
+            ${this.state.map(({ text }) => `<li>${text}</li>`).join('')}
         </ul>
         `;
     };
