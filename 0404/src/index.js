8fad9f8a59ef591f097e3f055c0dd234517b7bd8
mieumje <66072832+mieumje@users.noreply.github.com>
2022-04-04 14:15:35 +0900

    0404_Simple Todo List Rendering

---
+++ 0404/src/index.js
@@ -0,0 +1,15 @@
+const data = [
+    {
+        text: "Javscript 학습"
+    },
+    {
+        text: "함수형 프로그래밍 강의"
+    },
+];
+
+const $app = document.querySelector('.app');
+
+new TodoList({
+    $target: $app,
+    initialState: data
+});
