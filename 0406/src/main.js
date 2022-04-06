c4e7bc6cd084218ae44321b56d4b1a9df6e31811
mieumje <66072832+mieumje@users.noreply.github.com>
2022-04-06 16:06:42 +0900

    0406_fetch api 사용하기 2
    상품 옵션 목록 조회 api, 상품 재고 조회 api를 이용해 더미데이터 형식으로 가공하기

--- 0406/src/main.js
+++ 0406/src/main.js
@@ -1,29 +1,44 @@
 import ProductOptions from "./ProductOptions.js"
+import {request} from "./api.js";
 
-const dummyData = [
-    {
-        optionId: 1,
-        optionName: "더미 데이터1",
-        optionPrice: 30000,
-        stock: 10
-    },
-    {
-        optionId: 2,
-        optionName: "더미 데이터2",
-        optionPrice: 24500,
-        stock: 2
-    },
-    {
-        optionId: 3,
-        optionName: "더미 데이터3",
-        optionPrice: 15000,
-        stock: 0
-    }
-];
 const $target = document.querySelector("#app");
-new ProductOptions({
+const fetchOptionData = (productId) => {
+    return request(`/products/${productId}`)
+        .then(product => {
+            return request(`/product-options?product.id=${product.id}`)
+        })
+        .then(productOptions => {
+            return Promise.all([
+                Promise.resolve(productOptions),
+                Promise.all(
+                    productOptions.map(productOption => productOption.id).map(id => {
+                        return request(`/product-option-stocks?productOption.id=${id}`)
+                    })
+                )
+            ])
+        })
+        .then(data => {
+            const [productOptions, stocks] = data;
+            const optionDtata = productOptions.map((productOption, index) => {
+                const stock = stocks[index][0].stock
+
+                return {
+                    optionid: productOption.id,
+                    optionName: productOption.optionName,
+                    optionPrice: productOption.optionPrice,
+                    stock
+                }
+            })
+            
+            console.log(optionDtata)
+            productOptionsComponent.setState(optionDtata);
+        })
+        
+}
+fetchOptionData(1);
+const productOptionsComponent = new ProductOptions({
     $target,
-    initialState: dummyData,
+    initialState: [],
     onSelect: (option) => {
         alert(option.optionName)
     }
