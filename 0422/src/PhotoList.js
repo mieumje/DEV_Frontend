1f6f16969d0a5c8792620eacb7328d74d5ef635e
mieumje <66072832+mieumje@users.noreply.github.com>
2022-04-22 17:54:24 +0900

    0422_무한 스크롤 : LoadMore를 통해 새로운 데이터 추가 시 수정
    모든 데이터를 새로 렌더링 하는 것이 아니라 id 기준으로 렌더링 체크 후 새로 appendChild

--- 0422/src/PhotoList.js
+++ 0422/src/PhotoList.js
@@ -3,6 +3,7 @@ export default function PhotoList({
   initialState,
   onScrollEnded
 }){
+  let isInitiallize = false;
   const $photoList = document.createElement('div');
   $target.appendChild($photoList);
   this.state = initialState;
@@ -13,19 +14,27 @@ export default function PhotoList({
   };
 
   this.render = () => {
-    const { photoList } = this.state;
-    $photoList.innerHTML = `
-      ${this.state.map(photo => {
-        return `
-        <ul>
-          <li style="list-style:none;">
-            <img width="100%" src="${photo.imagePath}" />
-          </li>
-        </ul>
-        `
-      }).join('')}
-      <button class="PhotoList_LoadMore" style="width: 100%; height: 20px; font-size=20px;">Load More</button>
-    `
+    if (!isInitiallize) {
+      $photoList.innerHTML = `
+        <ul class="PhotoList_photos"></ul>
+        <button class="PhotoList_LoadMore" style="width: 100%; height: 20px; font-size=20px;">Load More</button>
+      `;
+      isInitiallize = true;
+    }
+
+    const $photos = $photoList.querySelector('.PhotoList_photos');
+    this.state.forEach(photo => {
+        // photo의 id 기준으로 렌더링이 되어 있는지 체크
+        if ($photos.querySelector(`li [data-id="${photo.id}"]`) === null) {
+          // 없으면 li 생성하고 $photos에 appendChild
+          const $li = document.createElement('li');
+          $li.setAttribute('data-id', photo.id);
+          $li.style = 'list-style: none;';
+          $li.innerHTML =`<img width="100%" src="${photo.imagePath}" />`;
+
+          $photos.appendChild($li);
+        }
+    });
   };
 
   $photoList.addEventListener('click', e => {
