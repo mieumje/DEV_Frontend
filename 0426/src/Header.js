f3508d574d31c5b47a301bf9300bbfa58d1bd2d4
mieumje <66072832+mieumje@users.noreply.github.com>
2022-04-26 19:00:06 +0900

    0426_고양이 사진 검색기 만들기 : 기본 component 배치

---
+++ 0426/src/Header.js
@@ -0,0 +1,19 @@
+import Keyword from "./Keyword.js";
+
+export default function Header({
+  $target,
+}){
+  const $header = document.createElement('header');
+  $header.className = 'Header';
+
+  $target.appendChild($header);
+
+  const $title = document.createElement('h1');
+  $title.style.textAlign = 'Center';
+  $title.innerHTML = '고양이 사진 검색기';
+  $header.appendChild($title);
+
+  const keyword = new Keyword({
+    $target: $header,
+  });
+}
---
+++ 0426/src/Keyword.js
@@ -0,0 +1,8 @@
+export default function Keyword({
+  $target,
+}){
+  const $keyword = document.createElement('input');
+  $keyword.className = 'Keyword';
+
+  $target.appendChild($keyword);
+}
