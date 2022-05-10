f038c366992069d4913beb3161ac2f09f8d68b5a
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-10 19:57:02 +0900

    0510_SFC 프로젝트 구성 : w. Webpack 2
    하위 컴포넌트 생성
    style scoped 적용

--- 0510/vue_start_webpack/src/App.vue
+++ 0510/vue_start_webpack/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <h1>{{ msg }}</h1>
+  <Hello />
 </template>
 
 <script>
+import Hello from './components/Hello.vue';
+
 export default {
+  components: {
+    Hello: Hello,
+  },
   data() {
     return {
-      msg: "hello webpack@"
+      msg: "hello webpack.."
     }
   }
 }
 </script>
-
-<style>
-h1 {
-  color: red;
-}
-</style>
