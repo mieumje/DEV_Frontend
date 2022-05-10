2afaa215ff419e5cdd836cce961efb8c885b515d
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-10 13:54:58 +0900

    0510_컴포넌트 기초 : $emit
    하위 컴포넌트의 데이터는 readonly라서 수정이 불가능하다.
    
    데이터를 수정하기 위해서는 최상위 컴포넌트(데이터를 보내준)에서 수정을 해야 하는데, 하위 컴포넌트에서 요청을 보내야 한다.
    
    $emit(커스텀 이벤트, 수정할 데이터)
    커스텀 이벤트가 하위 컴포넌트에서 발생하게 되면, 커스텀 이벤트에 연결이된 메서드에 두 번째 인자의 데이터를 전송한다.

--- 0510/index.js
+++ 0510/index.js
@@ -7,12 +7,23 @@ const App = {
         { id: '3', name: 'cherry'}
       ]
     }
+  },
+  methods: {
+    toUpper(upperName) {
+      console.log(upperName)
+    }
   }
 };
 
 const app = Vue.createApp(App);
 app.component('upper-name', {
-  template: `<div>{{ name }}</div>`,
-  props: ['name']
+  template: `<div @click="capitalize">{{ name }}</div>`,
+  props: ['name'],
+  methods: {
+    capitalize() {
+      // this.name = this.name.toUpperCase() // 하위 컴포넌트의 데이터는 readOnly라서 수정이 불가능.
+      this.$emit('to-upper', this.name.toUpperCase())  // 수정을 하겠다는 요청을 외부(상위 컴포넌트)로 보내준다.
+    }
+  }
 })
 const vm = app.mount('#app');
