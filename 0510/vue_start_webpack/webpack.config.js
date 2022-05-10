79cc49bfb42c062681685ac725244ec433d7777d
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-10 20:35:40 +0900

    0510_SFC 프로젝트 구성 : w. Webpack 2
    favicon 추가
    
    npm i -D copy-webpack-plugin
    
    html에서 favicon.icon을 사용하겠다고 하지 않더라도 자동으로 favicon을 찾아 적용한다.

--- 0510/vue_start_webpack/webpack.config.js
+++ 0510/vue_start_webpack/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
 const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   resolve: {
@@ -34,9 +35,14 @@ module.exports = {
     new VueLoaderPlugin(),
     new HtmlPlugin({
       template: './src/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'static' }
+      ]
     })
   ],
   devServer: {
-    port: 1234,
+    port: 1111,
   }
 }
