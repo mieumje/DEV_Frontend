6efe85c485a0334f02a1ccb98754cb3b21337fc6
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-11 17:10:46 +0900

    0511_initial commit

---
+++ 0511/vue_start_webpack/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>
+      {{ msg }}
+    </h1>
+    <Hello />
+  </div>
+</template>
+
+<script>
+import Hello from '~/components/Hello';
+
+export default {
+  components: {
+    Hello: Hello,
+  },
+  data() {
+    return {
+      msg: 'hello webpack..'
+    };
+  }
+};
+</script>
