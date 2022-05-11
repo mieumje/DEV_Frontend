25896b0ab5f7d7c83be026e5cf48109131b9a16e
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-11 17:25:18 +0900

    0511_컴포넌트 등록 : 전역 컴포넌트 등록
    일반적으로 지역 등록을 사용하고
    
    버튼과 같은 자주 사용되고 언제든지 쓸 수 있는 컴포넌트들 만 전역등록을 한다.

--- 0511/vue_start_webpack/src/App.vue
+++ 0511/vue_start_webpack/src/App.vue
@@ -1,23 +1,13 @@
 <template>
   <div>
-    <h1>
-      {{ msg }}
-    </h1>
-    <Hello />
+    <h1>Hello Vue!</h1>
+    <Btn />
   </div>
 </template>
 
 <script>
-import Hello from '~/components/Hello';
 
 export default {
-  components: {
-    Hello: Hello,
-  },
-  data() {
-    return {
-      msg: 'hello webpack..'
-    };
-  }
+  
 };
 </script>
--- 0511/vue_start_webpack/src/main.js
+++ 0511/vue_start_webpack/src/main.js
@@ -1,4 +1,9 @@
-import * as Vue from 'vue';
-import App from './App.vue';
+import { createApp } from 'vue';
+import App from '~/App';
+import Btn from '~/components/Btn'; // Btn 컴포넌트를 전역 객체로 등록
 
-Vue.createApp(App).mount('#app');
+const app = createApp(App);
+
+app.component('Btn', Btn); // 옵션을 등록하는 부분에 컴포넌트를 등록
+
+app.mount('#app');
