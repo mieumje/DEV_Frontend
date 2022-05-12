deb1e67e7c194c6841bb9591cbf664fee9d87baa
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-13 03:10:47 +0900

    0512_Vuex(Store) : initial commit

---
+++ 0512/vue_start_webpack_Store/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- eslint-disable -->
+  <h1>{{ msg }}</h1>
+  <Hello />
+  <!-- eslint-enable -->
+</template>
+
+<script>
+import Hello from '~/components/Hello';
+
+export default {
+  components: {
+    Hello
+  },
+  data() {
+    return {
+      msg: 'Hello Vue!'
+    };
+  }
+};
+</script>
---
+++ 0512/vue_start_webpack_Store/src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>hello vue!</h1>
+</template>
+
+<script>
+
+export default {
+  
+};
+</script>
+
+<style scoped lang="scss">
+$color: skyblue;
+h1 {
+  color: $color;
+}
+</style>
