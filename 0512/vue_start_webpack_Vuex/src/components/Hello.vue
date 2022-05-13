d0b9dbc459a2f57f4de2ea1133bafd4849f1731d
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-13 18:20:46 +0900

    0512_Vuex
    computed, methods를 따로 지정해 사용할 수 있지만
    이런 내용들이 많아지면 쉽게 제어할 수 있는 방법이 있는데 그 방법이 mapping 기능이다.
    
    computed(계산된 데이터)를 가져올 때, 개별적으로 state, getters, mutations, actions를 가져와 등록하는 방법도 자주 사용하지만
    가져올 내용이 많은 경우 map이 붙어있는 메서드를 통해 한번에 매핑할 수 있다.
    
    map으로 시작하는 vuex 패키지에서 가져온 메서드를 통해 매핑하면 양이 많아지는 경우 편리하게 사용할 수 있다.

--- 0512/vue_start_webpack_Vuex/src/components/Hello.vue
+++ 0512/vue_start_webpack_Vuex/src/components/Hello.vue
@@ -9,33 +9,27 @@
 </template>
 
 <script>
+import { mapState, mapGetters, mapMutations, mapActions} from 'vuex';
 export default {
-  data() {
-    return {
-      
-    };
-  },
   computed: {
-    count() {
-      return this.$store.state.count.count;
-    },
-    message() {
-      return this.$store.state.message.message;
-    },
-    reversedMessage() {
-      return this.$store.getters['message/reversedMessage'];
-    }
+    ...mapState('count', [  // 첫 번째 매개변수로 사용할 모듈의 namespaced를 작성, 두 번째 매개변수로 사용할 모듈의 데이터를 배열 데이터로 작성
+      'count'
+    ]), // mapState 함수가 실행된 결과를 전개한다.
+    ...mapState('message', [
+      'message'
+    ]),
+    ...mapGetters('message', [
+      'reversedMessage'
+    ])
   },
   methods: {
-    increaseCount() {
-      this.$store.commit('count/increaseCount');
-    },
-    decreaseCount() {
-      this.$store.commit('count/decreaseCount');
-    },
-    fetchTodo() {
-      this.$store.dispatch('message/fetchTodo');
-    }
+    ...mapMutations('count', [
+      'increaseCount',
+      'decreaseCount'
+    ]),
+    ...mapActions('message', [
+      'fetchTodo'
+    ])
   }
 };
 </script>
