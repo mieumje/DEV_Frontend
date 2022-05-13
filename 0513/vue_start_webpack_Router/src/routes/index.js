63022b124d6f03c5e69432e53a19070cef9db9ee
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-13 20:07:36 +0900

    0512_Vue Router 기초 : NotFound 처리
    routes 옵션에 원하는 경로를 등록하고, 그 외의 경로를 /:notFound()를 통해 등록한다.
    
    등록되지 않은 경로로 들어가는 경우 보여줄 페이지 NotFound.vue를 생성하고 화면에 출력되도록 등록한다.

--- 0513/vue_start_webpack_Router/src/routes/index.js
+++ 0513/vue_start_webpack_Router/src/routes/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory} from 'vue-router';
 import Home from './Home';
 import About from './About';
+import NotFound from './NotFound';
 
 export default createRouter({
   history: createWebHashHistory(),
@@ -12,6 +13,10 @@ export default createRouter({
     {
       path: '/about',
       component: About
+    },
+    {
+      path: '/:notFound(.*)', // 지정한 경로를 제외한 나머지 경로에 해당
+      component: NotFound // 지정한 경로를 제외한 경로에 들어섰을 때 보여줄 컴포넌트
     }
   ]
 });
