5c2dee1f35d37e25bce2ba79e2044fea87013c7e
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-13 18:23:04 +0900

    0512_Vuex
    전역으로 등록된 state는 namespaced를 사용하지 않고(모듈의 이름을 명시하는) 바로 가져오면 된다.
    
    index.js의 store의 state에 정의된 데이터 msg를 Hello 컴포넌트로 가져온다.

--- 0512/vue_start_webpack_Vuex/src/components/Hello.vue
+++ 0512/vue_start_webpack_Vuex/src/components/Hello.vue
@@ -6,12 +6,14 @@
   <button @click="decreaseCount">-</button>
   <div>{{ message }} / {{ reversedMessage }}</div>
   <button @click="fetchTodo">Update Message</button>
+  <div>{{ msg }}</div>
 </template>
 
 <script>
 import { mapState, mapGetters, mapMutations, mapActions} from 'vuex';
 export default {
   computed: {
+    ...mapState(['msg']), // 전역으로 등록된 state 데이터는 namespaced를 작성하지 않고 사용하면 된다.
     ...mapState('count', [  // 첫 번째 매개변수로 사용할 모듈의 namespaced를 작성, 두 번째 매개변수로 사용할 모듈의 데이터를 배열 데이터로 작성
       'count'
     ]), // mapState 함수가 실행된 결과를 전개한다.
