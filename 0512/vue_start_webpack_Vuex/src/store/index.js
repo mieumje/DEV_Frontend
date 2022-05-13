e6c5ea25e954b41a1c0160726bf1ca346ad8f5a6
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-13 16:30:06 +0900

    0512_Vuex
    $store는 main.js에서 store를 플러그인 등록했기 때문에 사용이가능하다.
    
    Hello 컴포넌트에서 state의 데이터를 가져와 사용할 때, data에 직접 등록해 사용하는 것이 아니라 계산된 데이터에 등록해 사용한다.

--- 0512/vue_start_webpack_Vuex/src/store/index.js
+++ 0512/vue_start_webpack_Vuex/src/store/index.js
@@ -1,16 +1,10 @@
 import { createStore } from 'vuex';
 
 export default createStore({ // 중앙 집중식 저장소(Store) 생성
-  state: {
-
-  },
-  getters: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  state() { // 데이터를 객체 데이터로 생성하면 참조 관계가 발생하므로 함수형으로 만든다.
+    return {
+      msg: 'Hello Vuex!',
+      count: 1
+    };
   }
 });
