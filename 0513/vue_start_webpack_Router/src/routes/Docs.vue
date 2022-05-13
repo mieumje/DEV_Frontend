9720ff36528540125d49f199c6961452121ab95b
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-13 20:33:44 +0900

    0513_Vue Router 기초 3 : children 옵션
    기존에 documents/id 값으로 경로를 이동하면 원하는 컴포넌트가 화면에 출력이 되었다.
    
    그러나, documents 경로로 들어가면 대응하는 컴포넌트가 없어 404 페이지가 출력되었다.
    
    이를 해결하기 위해, documents 에 대한 경로를 지정하고, documents/id 에 대한 경로를 children으로 지정해준다.
    
    children에서 하위 경로를 지정할 때에는 '/' 기호를 생략한다.
    
    하위 경로를 지정했을 때, 하위 경로에 해당하는 내용이 출력될 RouterView 템플릿을
    상위 경로의 컴포넌트에서 지정해 주어야 한다.

--- 0513/vue_start_webpack_Router/src/routes/Docs.vue
+++ 0513/vue_start_webpack_Router/src/routes/Docs.vue
@@ -1,4 +1,3 @@
 <template>
   <h1>Docs.vue</h1>
-  <h2>{{ $route.params.id }}</h2>
 </template>
--- 0513/vue_start_webpack_Router/src/routes/index.js
+++ 0513/vue_start_webpack_Router/src/routes/index.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import About from './About';
 import NotFound from './NotFound';
 import Docs from './Docs';
+import DocsId from './DocsId';
 
 export default createRouter({
   history: createWebHashHistory(),
@@ -16,8 +17,14 @@ export default createRouter({
       component: About
     },
     {
-      path: '/documents/:id',  // 콜론 기호를 사용하고 내용을 작성하면
-      component: Docs
+      path: '/documents',
+      component: Docs,
+      children: [
+        {
+          path: ':id',  // children 경로에서는 '/'를 사용하면 안된다. '/'는 루트 경로를 의미함.
+          component: DocsId
+        }
+      ]
     },
     {
       path: '/:notFound(.*)', // 지정한 경로를 제외한 나머지 경로에 해당
