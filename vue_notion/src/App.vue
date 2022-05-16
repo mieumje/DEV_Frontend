1e62a93b71842d882ed96b8dc3bc142aad5ee452
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-16 23:46:27 +0900

    노션 클론 1 : 레이아웃 설정
    reset css
    google fonts noto

--- vue_notion/src/App.vue
+++ vue_notion/src/App.vue
@@ -1,5 +1,48 @@
 <template>
   <!-- eslint-disable -->
-  <RouterView />
+  <div class="app__inner">
+    <div class="app__nav"></div>
+    <div class="app__page">
+      <div class="page__header"></div>
+      <div class="page__container">
+        <RouterView />
+      </div>
+    </div>
+  </div>
   <!-- eslint-enable -->
 </template>
+<style scoped lang="scss">
+* {
+  border: 1px solid;
+  box-sizing: border-box;
+}
+
+.app__inner {
+  display: flex;
+  height: 100vh;
+  font-size: 14px;
+  line-height: 1.4;
+  font-family: 'Noto Sans KR', sans-serif;
+
+  .app__nav {
+    width: 240px;
+    height: 100%;
+  }
+
+  .app__page {
+    flex-grow: 1;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+
+    .page__header {
+      height: 80px;
+    }
+
+    .page__container {
+      flex-grow: 1;
+      overflow-y: auto;
+    }
+  }
+}
+</style>
