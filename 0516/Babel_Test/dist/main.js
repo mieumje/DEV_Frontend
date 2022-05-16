690474c02727ca1b64bd3e641e420bca2a42dc79
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-16 18:40:01 +0900

    0516_Babel 구성
    @babel/plugin-transform-runtime
    @babel/runtime-corejs3 설치
    
    @babel/preset-env는 최신의 문법을 구형의 문법으로 변환해주는데,
    변환된 결과가 전역으로 등록되어 전역이 오염이 될 수 있다.
    
    해당 단점을 보완하기위해 @babel/plugin-transform-runtime 플러그인을 설치해
    전역을 오염시키지 않고 변환된 결과가 동작하도록 한다.
    
    @babel/plugin-transform-runtime은 작성한 코드만 변환해 변환 결과의 용량도 줄일 수 있다.
    
    ["@babel/plugin-transform-runtime", {}] {} 부분에 옵션도 작성할 수 있다.

--- 0516/Babel_Test/dist/main.js
+++ 0516/Babel_Test/dist/main.js
@@ -1,8 +1,15 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime-corejs3/helpers/interopRequireDefault");
+
+var _includes = _interopRequireDefault(require("@babel/runtime-corejs3/core-js-stable/instance/includes"));
+
 var c = {};
 var d = c.name;
 
 var increase = function increase(num) {
   return num + 2;
 };
+
+var arr = [];
+(0, _includes["default"])(arr).call(arr, 'a');
--- 0516/Babel_Test/main.js
+++ 0516/Babel_Test/main.js
@@ -1,5 +1,7 @@
 const c = {};
 const d = c.name;
 
-
 const increase = num => num + 2
+
+const arr = []
+arr.includes('a')
