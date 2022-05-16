3e94f9d857dd5e8d6dd1fb9be7daed0b654b44a8
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-16 17:49:48 +0900

    0516_Vue Router 스크롤 동작 : initial commit

---
+++ 0516/vue_start_webpack_vue_router_scroll/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- eslint-disable -->
+  <RouterLink to="/">
+    Home
+  </RouterLink>
+  <RouterLink to="/about">
+    About
+  </RouterLink>
+  <RouterView />
+  <!-- eslint-enable -->
+</template>
+
+<script>
+
+
+export default {
+
+};
+</script>
---
+++ 0516/vue_start_webpack_vue_router_scroll/src/routes/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from './Home';
+import About from  './About';
+
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/about',
+      component: About
+    }
+  ]
+});
