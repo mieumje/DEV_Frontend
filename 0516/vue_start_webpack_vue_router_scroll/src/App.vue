2a03f9a30717204f5450ca415795bd230f790325
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-16 18:00:55 +0900

    0516_Vue Router 스크롤 동작
    뷰포트 내에서 스크롤이 어느정도 되어있는 상태에서 페이지 이동을 하게되면,
    스크롤이 유지가 된 상태로 페이지 이동이 발생한다.
    
    SPA 프로젝트는 index.html 하나의 파일을 기준으로 동작한다.
    
    단일 페이지가 동작하는 것이라 스크롤이 페이지가 달라져도 유지가 되는 문제가 있다.
    
    문제를 해결하기 위해 Vue Router에는 스크롤 동작을 제어하는 기능을 제공한다.
    
    scrollBehavior() 메서드를 통해 페이지 이동이 있는 경우 스크롤의 위치를 지정할 수 있다.
    
    top: 0으로 지정하면 스크롤이 최상단으로 맞춰진다.

--- 0516/vue_start_webpack_vue_router_scroll/src/App.vue
+++ 0516/vue_start_webpack_vue_router_scroll/src/App.vue
@@ -1,19 +1,38 @@
 <template>
   <!-- eslint-disable -->
-  <RouterLink to="/">
-    Home
-  </RouterLink>
-  <RouterLink to="/about">
-    About
-  </RouterLink>
-  <RouterView />
+  <header>
+    <RouterLink to="/">
+      Home...
+    </RouterLink>
+    <RouterLink to="/about">
+      About...
+    </RouterLink>
+  </header>
+  <section>
+    <RouterView />
+  </section>
   <!-- eslint-enable -->
 </template>
 
 <script>
-
-
 export default {
 
 };
 </script>
+
+<style scoped lang="scss">
+header {
+  background-color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+}
+
+section {
+  margin-top: 80px;
+}
+</style>
