b3b521fb8ca3667778cd8b842e7ae13105632a70
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-17 01:30:41 +0900

    노션 클론 2 : nav item
    arrow span을 click 하면 showCildren 데이터를 true/false 전환
    
    showChildren이 true인 경우, 하위 item을 나타내는 ul tag display flex 지정
    
    title이 비어 있는 경우 '제목 없음'으로 초기화
    
    arrow span 클릭 시 active 속성이 활성화 되면 transform 애니메이션 추가

--- vue_notion/src/components/WorkspaceItem.vue
+++ vue_notion/src/components/WorkspaceItem.vue
@@ -3,11 +3,14 @@
     <div 
       :style="{ paddingLeft: `${14 * depth}px`}"
       class="title">
-      <span class="material-icons">
+      <span
+        :class="{ active: showChildren }"
+        class="material-icons"
+        @click="showChildren = !showChildren">
         play_arrow
       </span>
       <span class="text">
-        {{ workspace.title }}
+        {{ workspace.title || '제목 없음' }}
       </span>
       <div class="actions">
         <span class="material-icons">
@@ -18,7 +21,13 @@
         </span>
       </div>
     </div>
-    <ul v-if="hasChildren">
+    <div
+      v-if="!hasChildren && showChildren"
+      :style="{ paddingLeft : `${14 * depth + 22}px`}"
+      class="no-children">
+      하위 페이지가 없습니다.
+    </div>
+    <ul v-if="hasChildren && showChildren">
       <WorkspaceItem
         v-for="ws in workspace.documents"
         :key="ws.id"
@@ -39,6 +48,11 @@ export default {
       type: Number,
       default: 1
     }
+  },
+  data() {
+    return {
+      showChildren: false
+    };
   },
   computed: {
     hasChildren() {
@@ -72,6 +86,10 @@ li {
       &:hover {
         background-color: $color-background--hover2;
       }
+
+      &.active {
+        transform: rotate(90deg);
+      }
     }
 
     .text {
@@ -85,6 +103,16 @@ li {
       display: none;
       align-items: center;
     }
+  }
+
+  .no-children {
+    color: rgba($color-font, .35);
+    height: 30px;
+    display: flex;
+    align-items: center;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
   }
 }
 </style>
