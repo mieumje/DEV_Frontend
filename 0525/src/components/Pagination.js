90d7203e2ecdd5ac870654d4503972a17ac7c6bc
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-25 17:42:02 +0900

    0525_사용 사례-페이지네이션 2
    페이지네이션 버튼에 대한 이벤트 바인딩
    
    Prev, Next 버튼에 대한 이벤트 처리
    
    곱연산을 통해 예외처리
    Prev의 경우 0번 페이지가 아닌 경우만 이벤트 동작
    Next의 경우 현재 페이지에서 1을 증가시킨 값이 totalPage와 같지 않은 경우만 이벤트 동작

--- 0525/src/components/Pagination.js
+++ 0525/src/components/Pagination.js
@@ -3,14 +3,18 @@ const Pagination = ({ defaultPage, limit, total }) => {
   const [page, setPage] = useState(defaultPage);
   const totalPage = Math.ceil(total / limit);
 
+  const handleChangePage = (newPage) => {
+    setPage(newPage);
+  }
+
   return (
     <div>
-      <button>Prev</button>
+      <button onClick={() => page !==0 && handleChangePage(page - 1)}>Prev</button>
       {Array.from(new Array(totalPage), (_, i) => i)
         .map(i => (
           <button key={i} style={{ backgroundColor: page === i ? 'skyblue' : ''}}>{i + 1}</button>
       ))}
-      <button>Next</button>
+      <button onClick={() => page + 1 !== totalPage && handleChangePage(page + 1)}>Next</button>
     </div>
   )
 }
