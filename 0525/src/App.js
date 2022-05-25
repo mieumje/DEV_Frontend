bc41b4d6946aebe87a1c238bb259e1f74cb38f54
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-25 16:10:57 +0900

    0525_분기와 반복 1
    논리연산자와 삼항연산자를 통해 jsx 렌더링 여부 지정

--- 0525/src/App.js
+++ 0525/src/App.js
@@ -1,7 +1,17 @@
+import { useState} from 'react';
+
 function App() {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div>
-      React Start
+      <button onClick={() => setVisible(!visible)}>Toggle Me</button>
+      {visible && (
+        <h1>논리곱 연산자를 통해 쉽게 JSX 렌더링 여부를 결정할 수 있다.</h1>
+      )}
+      {visible ? (
+        <h1>삼항 연산자를 통해 쉽게 JSX 렌더링 여부를 결정할 수 있다.</h1>
+      ) : null}
     </div>
   );
 }
