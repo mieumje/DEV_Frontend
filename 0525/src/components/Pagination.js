de12bd87e8c5993ec95c39a6fba7fadddc084e9f
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-25 18:08:53 +0900

    0525_사용 사례-페이지네이션 5
    페이지네이션 컴포넌트의 모든 버튼이 노출되는 것이 아니라,
    5개씩 나오도록 filter 함수를 통해 지정한다.
    
    양 끝 족 페이지 번호에서의 예외 처리는 if - else if 조건문을 통해 지정한다.

--- 0525/src/components/Pagination.js
+++ 0525/src/components/Pagination.js
@@ -12,6 +12,14 @@ const Pagination = ({ defaultPage, limit, total, onChange }) => {
     <div>
       <button onClick={() => page !==0 && handleChangePage(page - 1)}>Prev</button>
       {Array.from(new Array(totalPage), (_, i) => i)
+        .filter(i => {
+          if (page < 2) {
+            return i < 5;
+          } else if (page > totalPage - 3) {
+            return i >= totalPage - 5;
+          }
+          return i >= page - 2 && i <= page + 2;
+        })
         .map(i => (
           <button key={i} onClick={() => handleChangePage(i)} style={{ backgroundColor: page === i ? 'skyblue' : ''}}>{i + 1}</button>
       ))}
