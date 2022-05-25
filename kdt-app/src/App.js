f4fd847fdba282cb633b2202cf6c495629691158
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-25 15:32:17 +0900

    0525_React : 컴포넌트
    App 컴포넌트 내부의 logo를 컴포넌트화

--- kdt-app/src/App.js
+++ kdt-app/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './components/Logo'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
