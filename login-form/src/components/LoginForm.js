0c052ea08c360bbf4e21dbbe618535d0e428b067
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-26 22:36:20 +0900

    login-form : LoginForm 컴포넌트 style 적용
    styled를 통해 Form과 Title 지정

--- login-form/src/components/LoginForm.js
+++ login-form/src/components/LoginForm.js
@@ -1,4 +1,18 @@
+import styled from "@emotion/styled";
 import Input from "./Input";
+
+const CardForm = styled.form`
+  padding: 16px;
+  width: 400px;
+  background-color: white;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
+  box-sizing: border-box;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  text-align: center;
+`
 
 const LoginForm = ({ onSubmit }) => {
   const handleSubmit = (e) => {
@@ -7,10 +21,12 @@ const LoginForm = ({ onSubmit }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Input type="text" />
+    <CardForm onSubmit={handleSubmit}>
+      <Title>Login</Title>
+      Name : <Input type="text" name="name"/>
+      Password : <Input type="password" name="password"/>
       <button type="submit">Login</button>
-    </form>
+    </CardForm>
   )
 };
 
