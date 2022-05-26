cc93a51d575c85c3e27efef3980f76f7c2f51fbf
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-26 16:19:50 +0900

    0526_컴포넌트 스타일링 : CSS in JS
    JS로 만들어진 Style코드가 CSS로 만들어지는 것이다.
    
    여러 라이브러리가 있다.
    emotion 라이브러리
    
    설치
    npm install --save @emotion/react
    npm install --save @emotion/styled
    npm install --save-dev @emotion/babel-plugin
    
    emotion을 사용하기 위해서 babel 플러그인도 설치해야 한다.
    
    다만, create-react-app을 통해 만든 프로젝트에서는 .babelrc가 적용되지 않는다.
    
    따라서 babelrc를 사용하지 않고 적용하기 위해서는 프래그마를 사용해야 한다.
    
    프래그마라는 것은 컴파일러에게 이 파일을 어떻게 처리할지를 알리는 것이다.
    
    /** @jsxImportSource @emotion/react */
    babel이 emotion css를 처리하기 위해 프래그마를 붙여준 것이다.

--- kdt-app-2/src/App.js
+++ kdt-app-2/src/App.js
@@ -1,8 +1,22 @@
+/** @jsxImportSource @emotion/react */
 import Box from "./components/Box";
+import { css } from '@emotion/react'
+
+const style = css`
+  color: hotpink;
+`
+
+const SomeComponent = ({ children }) => (
+  <div css={style}>
+    Some hotpink text.
+    {children}
+  </div>
+)
 
 function App() {
   return (
     <div>
+      <SomeComponent />
       <Box bgColor="red"/>
     </div>
   );
