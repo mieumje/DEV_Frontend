5b97286835ea6f3f275913a40f80bc3499739a80
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-26 16:40:53 +0900

    0526_useMemo 2
    useMemo를 통해 앞서 언급한 내용을 방지(다시 연산되는 것)할 수 있다.
    
    계산된 결과를 메모이제이션 하고, 특정 상태가 변경될 때만 다시 계산하도록 지정한다.
    
    label prop을 바꿀 때 마다sum함수가 계속 호출되지만
    useMemo를 사용하는 경우, n 상태는 변경되지 않아 sum 함수가 다시 실행되어 새로 연산을 하지 않게 된다.

--- kdt-app-2/src/components/ShowSum.js
+++ kdt-app-2/src/components/ShowSum.js
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+
 function sum(n) {
   console.log('Start');
   let result = 0;
@@ -9,7 +11,8 @@ function sum(n) {
   return result;
 }
 const ShowSum = ({ label, n }) => {
-  const result = sum(n);
+  // const result = sum(n);
+  const result = useMemo(() => sum(n), [n]) // 메모이제이션, 어떤 상태가 변경되었을 때 실행할 의존성 지정
   return(
     <span>
       {label} : {result}
