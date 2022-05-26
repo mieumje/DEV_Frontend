bdd302c8b7527c9c35f36242c8474dfe75b2c29c
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-26 22:24:01 +0900

    login-form : 기본 login-form 작성 2
    onSubmit이 잘 동작하는지 Storybook에서 확인

--- login-form/src/stories/LoginForm.stories.js
+++ login-form/src/stories/LoginForm.stories.js
@@ -3,6 +3,9 @@ import LoginForm from "../components/LoginForm";
 export default {
   title: 'Component/LoginForm',
   component: LoginForm,
+  argTypes: {
+   onSubmit: { action: "onSubmit" }, 
+  }
 };
 
 export const Default = (args) => {
