aa4e41b15385e6f76325f101f93a121af11ba3c1
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-26 22:33:16 +0900

    login-form : Input 컴포넌트 style 지정

--- login-form/src/components/Input.js
+++ login-form/src/components/Input.js
@@ -1,8 +1,15 @@
 import styled from '@emotion/styled'
 
 const Input = styled.input`
-  height: 24px;
-  background-color: white;
+  display: block;
+  padding: 6px 8px;
+  width: 100%;
+  height: 28px;
+  font-size: 14px;
+  border-radius: 4px;
+  border: 2px solid #333;
+  backgorund-color: white;
+  box-sizing: border-box;
 `
 
 export default Input;
