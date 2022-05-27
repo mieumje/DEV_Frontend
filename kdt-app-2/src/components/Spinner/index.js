808cf4da6839d5140b3ae887454fe8219bebd7cb
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-27 17:07:08 +0900

    0527_Spinner : 미완성

---
+++ kdt-app-2/src/components/Spinner/index.js
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+const Icon = styled.i`
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+const Spinner = ({ size = 24, color = '#919EAB', loading = true, ...props }) => {
+  const sizeStyle = {
+    width: size,
+    height: size
+  };
+  
+  return loading ? <Icon style={sizeStyle}>
+
+  </Icon> : null
+};
+
+export default Spinner;
