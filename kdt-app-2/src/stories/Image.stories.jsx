8e2081a3f96407e35aef9dc03773a3d9d1c74ab5
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-27 16:41:30 +0900

    0527_컴포넌트 : Image Lazy Loading

--- kdt-app-2/src/stories/Image.stories.jsx
+++ kdt-app-2/src/stories/Image.stories.jsx
@@ -4,11 +4,26 @@ export default {
   title: 'Component/Image',
   component: Image,
   argTypes: {
+    lazy: {
+      defaultValue: false,
+      control: { type: 'boolean'}
+    },
     src: {
       name: 'src',
       type: { name: 'string', require: true},
-      defaultValue: 'https://picsum.photos/200/',
+      defaultValue: 'https://picsum.photos/200',
       control: { type: 'text' }
+    },
+    placeholder: {
+      name: 'placeholder',
+      type: { name: 'string' },
+      defaultValue: 'https://via.placeholder.com/200',
+      control: { type: 'text' }
+    },
+    threshold: {
+      type: { name: 'number' },
+      defaultValue: 0.5,
+      control: { type: 'number'}
     },
     width: {
       name: 'width',
@@ -39,3 +54,15 @@ export default {
 export const Default = (args) => {
   return <Image {...args} />;
 };
+
+export const Lazy = (args) => {
+  return (
+    <div>
+      {
+        Array.from(new Array(20), (_, k) => k).map(i => (
+          <Image {...args} lazy block src={`${args.src}?${i}`} key={i}/>
+        ))
+      }
+    </div>
+  )
+}
