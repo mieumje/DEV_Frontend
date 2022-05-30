cb56e1fc00f5ae6b428d80dc9bbd2f52ab874607
mieumje <66072832+mieumje@users.noreply.github.com>
2022-05-30 16:56:40 +0900

    feat: TaskList 컴포넌트 생성
    기본 Todo Item인 Task를 담고 있는 컴포넌트.
    
    provider를 사용하기 전 기본 입력값으로 테스트

---
+++ todo/src/stories/TaskList.stories.jsx
@@ -0,0 +1,10 @@
+import TaskList from "../components/TaskList/index.js";
+
+export default {
+  title: 'Component/TaskList',
+  component: TaskList
+};
+
+export const Default = () => {
+  return <TaskList />;
+};
