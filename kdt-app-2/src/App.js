798d62ce76a3b5a474b0921931ff273f703e4ae7
mieumje <66072832+mieumje@users.noreply.github.com>
2022-06-08 20:58:16 +0900

    0608_react-router : Routes/Route
    Routes(v5에서는 Switch)로 한번 감싸주고, Route컴포넌트로 경로를 구분한다.
    
    path 경로에 따라 보여줄 컴포넌트를 element로 추가한다.
    
    v5에서는 다음과 같이 사용한다.
    <Route path="/경로">
      <하위컴포넌트 />
    </Route>

--- kdt-app-2/src/App.js
+++ kdt-app-2/src/App.js
@@ -34,53 +34,16 @@
 // };
 
 // export default App;
-
-import axios from 'axios';
-import { useAsync } from '@hooks';
-import { Header, Spinner } from '@components';
-import PostList from '@components/domain/PostList';
-import PostProvider from '@contexts/PostProvider';
-import { useCallback } from 'react';
-import PostAddForm from '@components/domain/PostAddForm';
-
-// 컴포넌트는 최대한 순수할수록 좋다.
-// 1. 사이드 이펙트를 걱정하지 않아도 된다.
-// 2. 확장에 유연하다.
-// 3. 테스트가 쉽다.
+import { Routes, Route } from "react-router-dom"; // v5에서는 Switch
 
 const App = () => {
-  const initialPosts =  useAsync(async () => {
-    return await axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => response.data);
-  }, []);
-
-  const handleAddPost = useCallback(async (post) => {
-    return await axios
-      .post(`https://jsonplaceholder.typicode.com/posts`, post)
-      .then((response) => response.data);
-  }, []);
-
-  const handleDeletePost = useCallback(async (id) => {
-    return await axios
-      .delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then(() => ({ id }));
-  }, []);
-
   return (
-    <PostProvider 
-      initialPosts={initialPosts.value} 
-      handleDeletePost={handleDeletePost}
-      handleAddPost={handleAddPost}
-    >
-      <Header>Posts</Header>
-      <PostAddForm />
-        {initialPosts.isLoading ? (
-          <Spinner />
-        ) : (
-          <PostList/>
-        )}
-    </PostProvider>
+    <div>
+      <Routes>
+        <Route path="/" exact element={<h1>Home</h1>} />
+        <Route path="/posts" exact element={<h1>Posts</h1>} />
+      </Routes>
+    </div>
   );
 };
 
---
+++ kdt-app-2/src/pages/PostsPage.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import { useAsync } from '@hooks';
+import { Header, Spinner } from '@components';
+import PostList from '@components/domain/PostList';
+import PostProvider from '@contexts/PostProvider';
+import { useCallback } from 'react';
+import PostAddForm from '@components/domain/PostAddForm';
+
+// 컴포넌트는 최대한 순수할수록 좋다.
+// 1. 사이드 이펙트를 걱정하지 않아도 된다.
+// 2. 확장에 유연하다.
+// 3. 테스트가 쉽다.
+
+const PostsPage = () => {
+  const initialPosts =  useAsync(async () => {
+    return await axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.data);
+  }, []);
+
+  const handleAddPost = useCallback(async (post) => {
+    return await axios
+      .post(`https://jsonplaceholder.typicode.com/posts`, post)
+      .then((response) => response.data);
+  }, []);
+
+  const handleDeletePost = useCallback(async (id) => {
+    return await axios
+      .delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(() => ({ id }));
+  }, []);
+
+  return (
+    <PostProvider 
+      initialPosts={initialPosts.value} 
+      handleDeletePost={handleDeletePost}
+      handleAddPost={handleAddPost}
+    >
+      <Header>Posts</Header>
+      <PostAddForm />
+        {initialPosts.isLoading ? (
+          <Spinner />
+        ) : (
+          <PostList/>
+        )}
+    </PostProvider>
+  );
+};
+
+export default PostsPage;
