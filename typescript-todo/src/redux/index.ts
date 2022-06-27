f4cecaf18007dd587ea2a693f6970d36c788401b
mieumje <66072832+mieumje@users.noreply.github.com>
2022-06-27 23:52:13 +0900

    feat: redux-devtools-extension 적용

--- typescript-todo/src/redux/index.ts
+++ typescript-todo/src/redux/index.ts
@@ -5,9 +5,10 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { tasks } from './tasks';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({ tasks });
 
-export const store = createStore(rootReducer, applyMiddleware(logger));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
 export type RootState = ReturnType<typeof rootReducer>;
