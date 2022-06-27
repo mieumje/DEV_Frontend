7de6b89caa1eb5b7dad1fd5898175cc6af6429b5
mieumje <66072832+mieumje@users.noreply.github.com>
2022-06-27 23:47:28 +0900

    feat: redux-logger 미들웨어를 적용
    상태가 변경될 때 콘솔에 로그를 찍어주는 미들웨어 라이브러리이다.

--- typescript-todo/src/redux/index.ts
+++ typescript-todo/src/redux/index.ts
@@ -2,11 +2,12 @@
 // 최상위 index.tsx에서 Provider로 App을 감싸고
 // 해당 Provider에 store 값을 연결해야 하는데,
 // reducer를 전부 합친 store를 만드는 곳이다.
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { tasks } from './tasks';
+import logger from 'redux-logger';
 
 const rootReducer = combineReducers({ tasks });
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(logger));
 
 export type RootState = ReturnType<typeof rootReducer>;
