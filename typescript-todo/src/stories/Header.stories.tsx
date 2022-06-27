1054a4b95c02a711c957ff1b1700c83a91383231
mieumje <66072832+mieumje@users.noreply.github.com>
2022-06-27 17:44:29 +0900

    feat: 테스트를 위한 Header 컴포넌트 생성

---
+++ typescript-todo/src/stories/Header.stories.tsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default {
+  title: 'Componenets/Header',
+  component: Header
+}
+
+export const Default = () => {
+  return <Header>Header</Header>;
+};
