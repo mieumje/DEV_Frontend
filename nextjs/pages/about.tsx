de4fec5b266ef1cb83276a7385d4484092ba1f2d
mieumje <66072832+mieumje@users.noreply.github.com>
2022-06-28 17:19:30 +0900

    feat: styled 컴포넌트 생성
    react에서 사용하듯 styled 컴포넌트를 적용할 수 있다.

--- nextjs/pages/about.tsx
+++ nextjs/pages/about.tsx
@@ -1,7 +1,16 @@
+import styled from "@emotion/styled";
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: red;
+`
+
 const About = () => {
   return (
     <div>
       About
+      <Box />
     </div>
   )
 }
