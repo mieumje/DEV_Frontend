6eed9de8e3e9d87eb9c6b28db3d5e21d2bd35b2b
mieumje <66072832+mieumje@users.noreply.github.com>
2022-08-25 16:41:41 +0900

    feat: Moc data todo list rendering

--- vanilaTodo/src/App.js
+++ vanilaTodo/src/App.js
@@ -1,8 +1,23 @@
 import Header from "./Header.js";
 
-export default function App({ $target, initialState }) {
+export default function App({ $target, initialState = [] }) {
+  this.state = initialState;
+
   new Header({
     $target,
     text: "Simple Todo List",
   });
+
+  const $todoList = document.createElement("div");
+  $target.appendChild($todoList);
+
+  this.render = () => {
+    $todoList.innerHTML = `
+      <ul>
+        ${this.state.map((todo) => `<li>${todo.text}</li>`).join("")}
+      </ul>
+    `;
+  };
+
+  this.render();
 }
