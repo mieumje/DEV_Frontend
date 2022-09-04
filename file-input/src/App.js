ca5f86d5b736080bd5b378f226caed5bacdbf2b8
mieumje <66072832+mieumje@users.noreply.github.com>
2022-09-04 23:00:28 +0900

    feat: 파일 업로드 예제(FileReader, createObjectURL)

--- file-input/src/App.js
+++ file-input/src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
+import FileInput from "./components/fileInput";
 
 function App() {
+  const [fileReaderThumbnail, setFileReaderThumbnail] = useState();
+  const [URLThumbnail, setURLThumbnail] = useState();
+
+  const encodeFile = (fileBlob) => {
+    const reader = new FileReader();
+
+    if (!fileBlob) return;
+
+    reader.readAsDataURL(fileBlob);
+
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        const result = reader.result;
+
+        setFileReaderThumbnail(result);
+
+        resolve();
+      };
+    });
+  };
+
+  const createImageURL = (fileBlob) => {
+    if (URLThumbnail) URL.revokeObjectURL(URLThumbnail);
+
+    const url = URL.createObjectURL(fileBlob);
+
+    setURLThumbnail(url);
+  };
+
+  const onFileReaderChange = (e) => {
+    const { files } = e.target;
+
+    if (!files || !files[0]) return;
+
+    const uploadImage = files[0];
+
+    encodeFile(uploadImage);
+  };
+
+  const onImageChange = (e) => {
+    const { files } = e.target;
+
+    if (!files || !files[0]) return;
+
+    const uploadImage = files[0];
+
+    createImageURL(uploadImage);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="section">
+        <h1>File Input by FileReader</h1>
+        <div className="image-wrapper">
+          {fileReaderThumbnail ? (
+            <img src={fileReaderThumbnail} alt="thumbnail" />
+          ) : (
+            "이미지 미리보기"
+          )}
+        </div>
+        <FileInput label="File Reader Upload" onChange={onFileReaderChange} />
+      </div>
+      <div className="section">
+        <h1>File Input by createObjectURL</h1>
+        <div className="image-wrapper">
+          {URLThumbnail ? (
+            <img src={URLThumbnail} alt="thumbnail" />
+          ) : (
+            "이미지 미리보기"
+          )}
+        </div>
+        <FileInput label="create object URL Upload" onChange={onImageChange} />
+      </div>
     </div>
   );
 }
