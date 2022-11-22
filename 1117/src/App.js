a3f46741cb6145e19502cff37886707a45297a02
mieumje <66072832+mieumje@users.noreply.github.com>
2022-11-22 19:01:51 +0900

    feat: apply task reducer

--- 1117/src/App.js
+++ 1117/src/App.js
@@ -1,23 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import counterAction from "./store/action/counterAction";
 import counterInitAction from "./store/action/counterInitAction";
+import taskAction from "./store/action/taskAction";
 
 function App() {
-  const count = useSelector((state) => state.count);
+  const count = useSelector((state) => {
+    const { counterReducer } = state;
+    const { count } = counterReducer;
+    return count;
+  });
+  const taskList = useSelector((state) => {
+    const { taskReducer } = state;
+    const { tasks } = taskReducer;
+    return tasks;
+  });
+
+  const [task, setTask] = useState("");
   const dispatch = useDispatch();
 
+  const handleChange = (e) => {
+    const { target } = e;
+    const { value } = target;
+
+    setTask(value);
+  };
+
+  const handleSubmit = () => {
+    if (!task || count === 0) {
+      console.error("Input task or count value");
+      return;
+    }
+    const checkDuplicate = taskList
+      .map((item) => {
+        return item.task;
+      })
+      .filter((item, index) => (item === task ? index : ""));
+
+    dispatch(taskAction("ADD", { task, count }));
+  };
+  useEffect(() => {
+    console.log(taskList);
+  }, [taskList]);
   return (
     <>
       <div>Redux Example</div>
-      <div>
-        <p>State Count : {count}</p>
-        <button onClick={() => dispatch(counterAction(1, "PLUS"))}>+</button>
-        <button onClick={() => dispatch(counterAction(1, "MINUS"))}>-</button>
-        <button onClick={() => dispatch(counterInitAction())}>
-          Initialize
-        </button>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <label htmlFor="input">Task</label>
+        <input type="text" onChange={handleChange} />
+        <div>Count : {count}</div>
+        <div>
+          <button onClick={() => dispatch(counterAction(1, "PLUS"))}>+</button>
+          <button onClick={() => dispatch(counterAction(1, "MINUS"))}>-</button>
+          <button onClick={() => dispatch(counterInitAction())}>
+            Initialize
+          </button>
+        </div>
+        <button onClick={handleSubmit}>submit</button>
       </div>
+      <ul>
+        {taskList.map((item) => {
+          const { task, count } = item;
+          return (
+            <li key={item.task}>
+              {task}:{count}
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
