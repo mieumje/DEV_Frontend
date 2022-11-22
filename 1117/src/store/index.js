e2db7cbf6aac3a5f415aa4add17be8232fc9fc52
mieumje <66072832+mieumje@users.noreply.github.com>
2022-11-22 19:01:32 +0900

    feat: combine reducers

--- 1117/src/store/index.js
+++ 1117/src/store/index.js
@@ -1,4 +1,7 @@
 import { combineReducers, createStore } from "redux";
 import { counterReducer } from "./reducer/counter";
+import { taskReducer } from "./reducer/tasks";
 
-export const store = createStore(counterReducer);
+const rootStore = combineReducers({ counterReducer, taskReducer });
+
+export const store = createStore(rootStore);
