83bf239e3a7fad33d91d596e304b4d422ca5c58b
mieumje <66072832+mieumje@users.noreply.github.com>
2022-11-22 17:19:12 +0900

    feat: apply counter reducer(plus, minus, init)

--- 1117/src/App.js
+++ 1117/src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import counterAction from "./store/action/counterAction";
+import counterInitAction from "./store/action/counterInitAction";
 
 function App() {
   const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
 
   return (
     <>
       <div>Redux Example</div>
       <div>
         <p>State Count : {count}</p>
-        <button>+</button>
-        <button>-</button>
+        <button onClick={() => dispatch(counterAction(1, "PLUS"))}>+</button>
+        <button onClick={() => dispatch(counterAction(1, "MINUS"))}>-</button>
+        <button onClick={() => dispatch(counterInitAction())}>
+          Initialize
+        </button>
       </div>
     </>
   );
