0bf0dc2974b1d4621ef253b918ed33c7a846cf31
mieumje <66072832+mieumje@users.noreply.github.com>
2022-11-23 17:55:55 +0900

    feat: apply Redux store at localStorage

--- 1117/src/store/index.js
+++ 1117/src/store/index.js
@@ -1,7 +1,15 @@
 import { combineReducers, createStore } from "redux";
 import { counterReducer } from "./reducer/counter";
 import { taskReducer } from "./reducer/tasks";
+import { setItem, getItem } from "../utils/storage";
 
 const rootStore = combineReducers({ counterReducer, taskReducer });
 
-export const store = createStore(rootStore);
+const persistedState = getItem("taskState");
+
+export const store = createStore(rootStore, persistedState);
+
+store.subscribe(() => {
+  const state = store.getState();
+  setItem("taskState", state);
+});
