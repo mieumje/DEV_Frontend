bbc2b1ffe0b9aa6f4cebee0878dfa9e31a6c652e
Park Min Je <mason@gridwiz.com>
2023-03-23 13:43:57 +0900

    feat: 절대 경로 시정

--- 2023/recoil-project/vite.config.ts
+++ 2023/recoil-project/vite.config.ts
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@src', replacement: path.resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '@layout',
+        replacement: path.resolve(__dirname, 'src/layout'),
+      },
+      {
+        find: '@pages',
+        replacement: path.resolve(__dirname, 'src/pages'),
+      },
+      {
+        find: '@apis',
+        replacement: path.resolve(__dirname, 'src/apis'),
+      },
+      {
+        find: '@utils',
+        replacement: path.resolve(__dirname, 'src/utils'),
+      },
+      {
+        find: '@assets',
+        replacement: path.resolve(__dirname, 'src/assets'),
+      },
+      {
+        find: '@hooks',
+        replacement: path.resolve(__dirname, 'src/hooks'),
+      },
+      {
+        find: '@router',
+        replacement: path.resolve(__dirname, 'src/router'),
+      },
+      {
+        find: '@interfaces',
+        replacement: path.resolve(__dirname, 'src/interfaces'),
+      },
+    ],
+  },
+});
