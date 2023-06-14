c91681a8f0b8eaf1f0d7c9fb78f1b4aeeb454495
Park Min Je <mason@gridwiz.com>
2023-06-14 16:14:44 +0900

    about, home 페이지 생성

---
+++ 2023/next-js-project/src/pages/about/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Div from '@components/Div';
+
+function About() {
+  return (
+    <div>
+      <h1>Next Project</h1>
+      <Div>About page</Div>
+    </div>
+  );
+}
+
+export default About;
---
+++ 2023/next-js-project/src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Div from '@components/Div';
+
+function Home() {
+  return (
+    <div>
+      <h1>Next Project</h1>
+      <Div>Home Page</Div>
+    </div>
+  );
+}
+
+export default Home;
