ef2d44c8a1742abdb36fc9aa7799985f74d947b1
Park Min Je <mason@gridwiz.com>
2023-08-28 13:39:47 +0900

    feat: AG Grid Theme(alpine, balham, material)

--- 2023/ag-grid/src/App.tsx
+++ 2023/ag-grid/src/App.tsx
@@ -4,6 +4,8 @@ import { AgGridReact } from "ag-grid-react";
 import "ag-grid-enterprise";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+import "ag-grid-community/styles/ag-theme-balham.css";
+import "ag-grid-community/styles/ag-theme-material.css";
 
 function App() {
   const [rowData] = useState([
@@ -23,12 +25,76 @@ function App() {
       style={{
         width: "100%",
         height: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        display: "grid",
+        gap: "0.5rem",
+        padding: "2rem",
+        gridTemplateColumns: "repeat(8, minmax(0, 1fr))",
+        gridTemplateRows: "repeat(8, minmax(0, 1fr))",
       }}
     >
-      <div className="ag-theme-alpine" style={{ width: "50%", height: "50%" }}>
+      <div
+        className="ag-theme-alpine"
+        style={{
+          color: "#fefefe",
+          fontSize: "1rem",
+          gridColumn: "span 4",
+          gridRow: "span 2",
+          padding: "1rem",
+        }}
+      >
+        <span>ag theme alpine</span>
+        <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+      </div>
+      <div
+        className="ag-theme-alpine-dark"
+        style={{
+          color: "#fefefe",
+          fontSize: "1rem",
+          gridColumn: "span 4",
+          gridRow: "span 2",
+          padding: "1rem",
+        }}
+      >
+        <span>ag theme alpine dark</span>
+        <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+      </div>
+      <div
+        className="ag-theme-balham"
+        style={{
+          color: "#fefefe",
+          fontSize: "1rem",
+          gridColumn: "span 4",
+          gridRow: "span 2",
+          padding: "1rem",
+        }}
+      >
+        <span>ag theme balham</span>
+        <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+      </div>
+      <div
+        className="ag-theme-balham-dark"
+        style={{
+          color: "#fefefe",
+          fontSize: "1rem",
+          gridColumn: "span 4",
+          gridRow: "span 2",
+          padding: "1rem",
+        }}
+      >
+        <span>ag theme balham dark</span>
+        <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+      </div>
+      <div
+        className="ag-theme-material"
+        style={{
+          color: "#fefefe",
+          fontSize: "1rem",
+          gridColumn: "span 4",
+          gridRow: "span 2",
+          padding: "1rem",
+        }}
+      >
+        <span>ag theme material</span>
         <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
       </div>
     </div>
