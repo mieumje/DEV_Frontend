47f55ef6875af7d2c8210a73a2508219d988841e
Park Min Je <mason@gridwiz.com>
2023-08-28 10:50:39 +0900

    feat: AG Grid 패키지 다운그레이드, 예제 작성

--- 2023/ag-grid/src/App.tsx
+++ 2023/ag-grid/src/App.tsx
@@ -1,7 +1,38 @@
 import "./App.css";
+import { useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-enterprise";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
 function App() {
-  return <></>;
+  const [rowData] = useState([
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 },
+  ]);
+
+  const [columnDefs] = useState([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+  ]);
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div className="ag-theme-alpine" style={{ width: "50%", height: "50%" }}>
+        <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- 2023/ag-grid/src/main.tsx
+++ 2023/ag-grid/src/main.tsx
@@ -1,5 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { LicenseManager } from "ag-grid-enterprise";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import "ag-grid-enterprise";
+
+LicenseManager.setLicenseKey(import.meta.env.VITE_AG_GRID_KEY);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
